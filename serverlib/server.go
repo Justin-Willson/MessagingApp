18304bf448b985cfca9db4720cd6cbeb298ad4b1
Justin Willson <jt.will.525@gmail.com>
2022-04-08 14:58:40 -0600

    Added ability to register a client on server

--- serverlib/server.go
+++ serverlib/server.go
@@ -4,6 +4,7 @@ import (
     "fmt"
 	"net"
 	"os"  
+	"strings"
 )
 
 const (
@@ -12,32 +13,58 @@ const (
     CONN_TYPE = "tcp"
 )
 
+type Client struct {
+	hostname string
+	port string
+}
+
 func StartServer() {
+	clients := make([]Client, 0)
+
 	l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
 		if err != nil {
 			fmt.Println("Error listening:", err.Error())
 			os.Exit(1)
 		}
 	defer l.Close()
+	fmt.Println("Running")
 
 	for {
 		conn, err := l.Accept()
 		if err != nil {
 			// handle error
 		}
-		go handleRequest(conn)
+		go handleRequest(conn, &clients)
 	}
 }
 
-func handleRequest(conn net.Conn) {
+func handleRequest(conn net.Conn, clientList *[]Client) {
 	buf := make([]byte, 1024)
 
 	_, err := conn.Read(buf)
 	if err != nil {
 	  fmt.Println("Error reading:", err.Error())
 	}
-	
-	conn.Write([]byte("Message received.\n"))
+	data := strings.Split(string(buf), ":")
+	fmt.Println(data[0])
+
+
+	//Register
+	if data[0] == "ADD" {
+		registerNewClient(conn, data[1], clientList)
+	}
+
+	//TODO: Transmit
+
+	//TODO: Close
 	
 	conn.Close()
+}
+
+func registerNewClient(conn net.Conn, port string, clientList *[]Client) {
+	//TODO: Make this thread safe
+	addr := conn.RemoteAddr()
+	data := strings.Split(addr.String(), ":")
+	client := Client{data[0], port}
+	*clientList = append(*clientList, client)
 }
